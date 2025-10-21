e3120e0e17ab2f779c3bad60c8cfa0995398b9b4
Valerrriy Orlov <wrestlbird@gmail.com>
2025-10-21 23:04:49 +0300

    feat:The urfave/cli v3 library is enabled. Implemented help output

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory;",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("launching the hexlet-path-size")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
