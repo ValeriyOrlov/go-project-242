3170c2d82e4814b2474f72342457cafbf7b3799f
Valerrriy Orlov <wrestlbird@gmail.com>
2025-10-21 23:55:30 +0300

    feat: implemented file size calculation

--- path_size.go
+++ path_size.go
@@ -1,1 +1,33 @@
-package path_size
+package code
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func GetSize(path string) string {
+	fi, err := os.Lstat(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var size int64
+	switch mode := fi.Mode(); {
+	case mode.IsRegular():
+		size = fi.Size()
+		/*case mode.IsDir():
+		files, err := os.ReadDir(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			if !file.IsDir() {
+				fmt.Println(file.Info())
+			}
+		}
+		*/
+	}
+	return fmt.Sprintf("size: %d b	path: %s", size, path)
+}
