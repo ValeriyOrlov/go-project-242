a30d4ebad7513c342f105911d4f3f621106a6c4b
Valerrriy Orlov <wrestlbird@gmail.com>
2025-10-25 15:41:38 +0300

    feat: added a flag for human-readable format

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -14,9 +14,26 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory;",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "human",
+				Usage: "human-readable sizes (auto-select unit)",
+			},
+		},
+
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			path := cmd.Args().Get(0)
-			fmt.Println(code.GetSize(path))
+			bytes, err := code.GetSize(path)
+
+			if len(cmd.LocalFlagNames()) > 0 {
+				flagName := cmd.LocalFlagNames()[0]
+				if err != nil {
+					return err
+				}
+				fmt.Println(code.FormatSize(bytes, flagName))
+			} else {
+				fmt.Println(code.FormatSize(bytes, ""))
+			}
 			return nil
 		},
 	}
